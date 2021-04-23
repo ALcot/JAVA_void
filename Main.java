dbffd2327db20175971607945e3ba4826072ffd4
ALcot <mbaaccarima@gmail.com>
2021-04-23 18:54:31 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    // printDataメソッドを呼び出す
+    printData();
+  }
+  
+  // printDataメソッドを定義する
+  public static void printData() {
+    System.out.println("私の名前はALcot Nellyです");
+  }
+}
